c3f0085d1caca488bde2dac63127f82661dc03e1
vincent7955168@gmail.com <ljjain0508@gmail.com>
2024-11-09 00:13:31 +0800

    fix webdriver autoclose

--- practice/main.py
+++ practice/main.py
@@ -1,22 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
 import os
 import time
 
 DRIVER_PATH = os.path.join(os.getcwd(), 'chromedriver-win64', 'chromedriver.exe')
 
-SERVEICE = Service(executable_path=DRIVER_PATH)
 
-OPTIONS = webdriver.ChromeOptions()
-
-URL = 'https://www.facebook.com/?locale=zh_TW'
+URL = 'https://www.facebook.com/'
 
 LOGIN_URL = 'https://www.facebook.com/login/'
 
+EMAIL = 'vincent7955168@gmail.com'
+
+PASSWORD = 'Eggsy7955168~'
+
 class Facebook_Crawler():
     def __init__(self):
-        self.driver = webdriver.Chrome(service=SERVEICE, options=OPTIONS)
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option('detach', True)
+        self.options.add_argument('--log-level=1') # https://stackoverflow.com/questions/78530683/created-tensorflow-lite-xnnpack-delegate-for-cpu-message-randomly-appears-when
+        self.driver = webdriver.Chrome(options=self.options) # 避免自動關閉瀏覽器
         self.login_state = -1
 
     def set_cookies(self):
@@ -24,9 +29,20 @@ class Facebook_Crawler():
 
     def login(self):
         if self.login_state == -1:
-            self.driver.get(LOGIN_URL)
+            self.driver.get(URL)
             print('開始登入')
-            time.sleep(10)
+            self.email = self.driver.find_element(By.ID, 'email')
+            self.email.clear()
+            self.email.send_keys(EMAIL)
+
+            self.password = self.driver.find_element(By.ID, 'pass')
+            self.password.clear()
+            self.password.send_keys(PASSWORD)
+
+            time.sleep(5)
+
+            self.submit = self.driver.find_element(By.NAME, 'login')
+            self.submit.click()
         else:
             print('已登入')
 
